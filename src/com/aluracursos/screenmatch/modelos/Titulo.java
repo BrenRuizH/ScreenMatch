95d8eb636ff112c12f769cd4b4b88d5d38e95865
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-05-20 17:23:56 -0600

    2024-05-20 Aprovechando el modelo para series

---
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,62 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+
+    private String nombre;
+    private int fechaLanzamiento;
+    private int duracionMinutos;
+    private boolean incluidoPlan;
+    private double sumaEvaluaciones;
+    private int totalEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public boolean isIncluidoPlan() {
+        return incluidoPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaLanzamiento(int fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public void setDuracionMinutos(int duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public void setIncluidoPlan(boolean incluidoPlan) {
+        this.incluidoPlan = incluidoPlan;
+    }
+
+    public int getTotalEvaluaciones() {
+        return totalEvaluaciones;
+    }
+
+    public void muestraFichaTecnica() {
+        System.out.println("El nombre de la pelicula es " + nombre);
+        System.out.println("Fue lanzada en " + fechaLanzamiento);
+        System.out.println("Duración en minutos es " + duracionMinutos);
+    }
+
+    public void evalua(double nota) {
+        sumaEvaluaciones += nota;
+        totalEvaluaciones ++;
+    }
+
+    public double calculaMedia() {
+        return sumaEvaluaciones / totalEvaluaciones;
+    }
+}
