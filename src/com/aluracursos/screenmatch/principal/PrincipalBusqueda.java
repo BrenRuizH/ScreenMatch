1c4990bc99a481b75e6adca6e1c6128c71f45e1e
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-05-22 08:59:37 -0600

    2024-05-22 Implementando la comunicación con el Web Service

---
+++ src/com/aluracursos/screenmatch/principal/PrincipalBusqueda.java
@@ -0,0 +1,23 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalBusqueda {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=5137789a"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
